062dc6eb7cb2965bac5a8c450c9c155e2eb36b0c
SavvyOsive <SavvyOsive@users.noreply.github.com>
2021-07-23 13:13:00 +0000

    Fix styling

--- tests/HandlerGeneratorTest.php
+++ tests/HandlerGeneratorTest.php
@@ -6,13 +6,11 @@ use Savvyosive\HandlerGenerator\HandlerGenerator;
 
 class HandlerGeneratorTest extends TestCase
 {
-
     /** @test */
     public function handler_generator_returns_string()
     {
         $handlerMessage = HandlerGenerator::handler()->generate();
 
-        $this->assertEquals('Functionality for generating handler',$handlerMessage);
+        $this->assertEquals('Functionality for generating handler', $handlerMessage);
     }
-
 }
